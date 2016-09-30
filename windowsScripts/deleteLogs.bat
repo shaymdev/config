4fbfe53c2702f990723966039daec6e3b40335e8
Shay Merrill <smerrill@mastercontrol.com>
2016-09-30 15:05:37 -0600

    ahk for saving window locations, trim up gvim, update deletelogs for current computer

--- windowsScripts/deleteLogs.bat
+++ windowsScripts/deleteLogs.bat
@@ -1,14 +1,13 @@
 @echo off
-REM cd c:\mastercontrol\sites\pub\installedServices
 
-For /D %%G in ("d:\mastercontrol\sites\%1\installedServices\*") do (
+For /D %%G in ("c:\mc\sites\%1\installedServices\*") do (
 	echo deleting all in %%G\WEB-INF\railo\logs\
 	for /f %%L in ('dir /b %%G\WEB-INF\railo\logs\') do (
 		del %%G\WEB-INF\railo\logs\%%L
 	)
 )
 
-For /D %%G in ("d:\mastercontrol\sites\%1\services\EnterpriseManagement\WEB-INF\railo\logs\") do (
+For /D %%G in ("c:\mc\sites\%1\services\EnterpriseManagement\WEB-INF\railo\logs\") do (
 	echo deleting all in %%G
 	for /f %%L in ('dir /b %%G') do (
 		del %%G%%L
@@ -16,14 +15,28 @@ For /D %%G in ("d:\mastercontrol\sites\%1\services\EnterpriseManagement\WEB-INF\
 )
 
 REM and for lucee
-For /D %%G in ("d:\mastercontrol\sites\%1\installedServices\*") do (
+For /D %%G in ("c:\mc\sites\%1\installedServices\*") do (
 	echo deleting all in %%G\WEB-INF\lucee\logs\
 	for /f %%L in ('dir /b %%G\WEB-INF\lucee\logs\') do (
 		del %%G\WEB-INF\lucee\logs\%%L
 	)
 )
 
-For /D %%G in ("d:\mastercontrol\sites\%1\services\EnterpriseManagement\WEB-INF\lucee\logs\") do (
+For /D %%G in ("c:\mc\sites\%1\services\EnterpriseManagement\WEB-INF\lucee\logs\") do (
+	echo deleting all in %%G
+	for /f %%L in ('dir /b %%G') do (
+		del %%G%%L
+	)
+)
+
+For /D %%G in ("c:\mc\sites\%1\server\webapps\ROOT\WEB-INF\lucee\logs\") do (
+	echo deleting all in %%G
+	for /f %%L in ('dir /b %%G') do (
+		del %%G%%L
+	)
+)
+
+For /D %%G in ("c:\mc\sites\%1\server\logs\") do (
 	echo deleting all in %%G
 	for /f %%L in ('dir /b %%G') do (
 		del %%G%%L
