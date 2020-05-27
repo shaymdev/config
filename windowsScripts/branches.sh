73e1d52066dbc1acb65b37ae6f4fba006379d810
Shay Merrill <shaymdev@gmail.com>
2020-05-27 13:43:12 -0600

    Add a little script to check out my branches of all my sites

---
+++ windowsScripts/branches.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd /c/mc/sites/
+
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d -regex '.*\/4.*');
+do
+	if [ -d $dir/.git ]
+	then
+		branch=$(git -C $dir rev-parse --abbrev-ref HEAD);
+		printf '%s\t\t%s\n' "$dir" $branch;
+	fi
+done
+
