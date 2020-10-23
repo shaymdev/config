0ddd4d99f56ebf25f6831a8f31dd3a814ccf6f20
Shay Merrill <shaymdev@gmail.com>
2020-10-23 12:42:24 -0600

    Visual search in vim config, fix branches and addAdmin scripts

--- windowsScripts/addAdmin.ps1
+++ windowsScripts/addAdmin.ps1
@@ -1,5 +1,5 @@
-$DomainName = Read-Host "Domain name:"
 $ComputerName = Read-Host "Computer name:"
+$DomainName = Read-Host "Domain name for user:"
 $UserName = Read-Host "User name:"
 $AdminGroup = [ADSI]"WinNT://$ComputerName/Administrators,group"
 $User = [ADSI]"WinNT://$DomainName/$UserName,user"
